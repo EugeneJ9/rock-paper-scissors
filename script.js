6036e48facd529130795c8fa779570db6c44bd46
EugeneJ9 <111596903+EugeneJ9@users.noreply.github.com>
2023-10-24 15:13:56 +0300

    Add funtion for a user choice and playRound function to compare choises

--- script.js
+++ script.js
@@ -9,5 +9,34 @@ function getComputerChoice() {
     return 'scissors';
   }
 }
+let computerSelection = getComputerChoice();
+console.log(computerSelection);
 
-console.log(getComputerChoice());
+function getPlayerChoice() {
+  let playerChoice = prompt("Please enter ‘Rock’, ‘Paper’ or ‘Scissors’").toLowerCase();
+  return playerChoice;
+}
+
+let playerSelection = getPlayerChoice();
+console.log(playerSelection);
+
+function playRound() {
+  if (computerSelection === playerSelection) {
+    return "Tie!"
+
+  } else if (computerSelection === 'rock' && playerSelection ==='scissors') {
+    return "You Lose! Rock beats scissors";
+  } else if (computerSelection === 'rock' && playerSelection ==='paper') {
+    return "You Win! Paper beats rock";
+  } else if (computerSelection === 'paper' && playerSelection ==='rock') {
+    return "You Lose! Paper beats rock";
+  } else if (computerSelection === 'paper' && playerSelection ==='scissors') {
+    return "You Win! Scissors beats paper";
+  } else if (computerSelection === 'scissors' && playerSelection ==='paper') {
+    return "You Lose! Scissors beats paper";
+  } else if (computerSelection === 'scissors' && playerSelection ==='rock') {
+    return "You Won! Rock beats scissors";
+  }
+}
+
+console.log(playRound(playerSelection, computerSelection))
